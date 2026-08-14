72cfde1797d95921c5449e5f3a07391f39e20c74
Leticia <estagio01@scenario.ind.br>
2026-08-14 15:48:23 -0300

    feat: adiciona botao de reset e avisa vencedor

--- app/src/main/java/br/edu/ifsp/scl/sc3047733/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3047733/trucoscoreboard/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.edu.ifsp.scl.sc3047733.trucoscoreboard
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.sc3047733.trucoscoreboard.databinding.ActivityMainBinding
 
@@ -19,55 +20,43 @@ class MainActivity : AppCompatActivity() {
         with (activityMainBinding) {
             btn1TimeA.setOnClickListener {
                 contadorPontosTimeA++
-
-                if (contadorPontosTimeA == 12) {
-                    zerarComponentes(this@MainActivity, this@with)
-                }
-                if (contadorPontosTimeA == 11) {
-                    mostrarMaoDeOnze("Time A")
-                }
-                pontuacaoTimeA.text = contadorPontosTimeA.toString()
+                verificaPontos("Time A", contadorPontosTimeA, pontuacaoTimeA)
             }
 
             btn1TimeB.setOnClickListener {
                 contadorPontosTimeB++
-                if (contadorPontosTimeB == 12) {
-                    zerarComponentes(this@MainActivity, this@with)
-                }
-                if (contadorPontosTimeB == 11) {
-                    mostrarMaoDeOnze("Time B")
-                }
-
-                pontuacaoTimeB.text = contadorPontosTimeB.toString()
+                verificaPontos("Time B", contadorPontosTimeB, pontuacaoTimeB)
             }
 
             btn3TimeA.setOnClickListener {
                 contadorPontosTimeA += 3
-
-                if (contadorPontosTimeA >= 12) {
-                    zerarComponentes(this@MainActivity, this@with)
-                }
-                if (contadorPontosTimeA == 11) {
-                    mostrarMaoDeOnze("Time A")
-                }
-                pontuacaoTimeA.text = contadorPontosTimeA.toString()
+                verificaPontos("Time A", contadorPontosTimeA, pontuacaoTimeA)
             }
 
             btn3TimeB.setOnClickListener {
                 contadorPontosTimeB += 3
-                if (contadorPontosTimeB >= 12) {
-                    zerarComponentes(this@MainActivity, this@with)
-                }
-                if (contadorPontosTimeB == 11) {
-                    mostrarMaoDeOnze("Time B")
-                }
-
-                pontuacaoTimeB.text = contadorPontosTimeB.toString()
+                verificaPontos("Time B", contadorPontosTimeB, pontuacaoTimeB)
             }
 
+            btnReiniciarJogo.setOnClickListener {
+                zerarComponentes()
+            }
         }
 
+    }
 
+    private fun verificaPontos(time: String, pontuacao: Int, texto: TextView) {
+        texto.text = pontuacao.toString()
+
+        if (pontuacao >= 12) {
+            avisarVencedor(time)
+            zerarComponentes()
+            return
+        }
+
+        if (pontuacao == 11) {
+            mostrarMaoDeOnze(time)
+        }
     }
     private fun mostrarMaoDeOnze(time: String) {
         androidx.appcompat.app.AlertDialog.Builder(this)
@@ -77,11 +66,19 @@ class MainActivity : AppCompatActivity() {
             .show()
     }
 
-    private fun zerarComponentes(activity: MainActivity, binding: ActivityMainBinding) {
-        activity.contadorPontosTimeA = 0
-        activity.contadorPontosTimeB = 0
-        binding.pontuacaoTimeA.text = activity.contadorPontosTimeA.toString()
-        binding.pontuacaoTimeB.text = activity.contadorPontosTimeB.toString()
+    private fun avisarVencedor(time: String) {
+        androidx.appcompat.app.AlertDialog.Builder(this)
+            .setTitle("Fim de Jogo")
+            .setMessage("$time venceu! Parabéns!")
+            .setPositiveButton("Finalizar partida", null)
+            .show()
+    }
+
+    private fun zerarComponentes() {
+        contadorPontosTimeA = 0
+        contadorPontosTimeB = 0
+        activityMainBinding.pontuacaoTimeA.text = "0"
+        activityMainBinding.pontuacaoTimeB.text = "0"
     }
 }
 
